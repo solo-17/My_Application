98331754d86e43675f4c42d9ddeca939b17e5b17
Solo <rohjain17@gmail.com>
2019-02-24 21:37:20 +0530

    V3

--- app/src/main/java/com/solo/my_application/LoginActivity.java
+++ app/src/main/java/com/solo/my_application/LoginActivity.java
@@ -2,13 +2,68 @@ package com.solo.my_application;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+    String msg = "Android: Main Activity ";
+    EditText userName, password;
+
+    private EditText getUserName() {
+        return this.userName;
+    }
+
+    private EditText getPassword() {
+        return this.password;
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        addListenerOnButtonLogin();
+        addListenerOnButtonSignUp();
+
+        Log.d(msg,"On Create");
     }
+
+    private void addListenerOnButtonLogin() {
+        final Context context = this;
+        Button button = (Button) findViewById(R.id.buttonLogin);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context,HomeActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    protected void addListenerOnButtonSignUp() {
+        final Context context = this;
+        Button button = (Button) findViewById(R.id.buttonCreateAccount);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context,SignupActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    /** Called when the activity is about to become visible. */
+    @Override
+    protected void onStart () {
+        super.onStart();
+        Log.d(msg,"On Start");
+    }
+
+
 }
--- app/src/main/java/com/solo/my_application/MainActivity.java
+++ app/src/main/java/com/solo/my_application/MainActivity.java
@@ -1,18 +1,68 @@
 package com.solo.my_application;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
-import android.widget.ImageView;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    String msg = "Android: Main Activity ";
 
+    /** Called when the activity is first created. */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        addListenerOnButton();
 
-        ImageView bcImageView = (ImageView) findViewById(R.id.imageViewbc);
-        bcImageView.setImageResource(R.drawable.bc_fruit_veg);
+        String status = "Connection Status" + String.valueOf(isNetworkAvailable());
+        //status.concat(String.valueOf(isNetworkAvailable()));
+        Log.d(msg,"On Create");
+        Log.d(msg,status);
     }
+
+    private void addListenerOnButton() {
+        final Context context = this;
+        Button button = (Button) findViewById(R.id.buttonStart);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context , LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    /** Called when the activity is about to become visible. */
+    @Override
+    protected void onStart () {
+        super.onStart();
+        Log.d(msg,"On Start");
+    }
+
+    private boolean isNetworkAvailable() {
+        boolean connected = false;
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        //NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        //return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+
+        if(connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).getState() == NetworkInfo.State.CONNECTED ||
+                connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI).getState() == NetworkInfo.State.CONNECTED) {
+            //we are connected to a network
+            connected = true;
+        }
+        else
+            connected = false;
+
+        return connected;
+    }
+
 }
